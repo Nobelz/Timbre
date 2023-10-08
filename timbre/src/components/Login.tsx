f5b93aa4d705dea504b0e2147ce65ac88e7e1af4
David Frost <82253398+davidhfrost@users.noreply.github.com>
2023-10-08 14:26:45 -0400

    components

---
+++ timbre/src/components/Login.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+const Login = () => {
+  return (
+    <div>
+      <Button variant="contained" color="primary">
+        Login with Spotify
+      </Button>
+      {/* Add form for manual login */}
+    </div>
+  );
+};
+
+export default Login;
