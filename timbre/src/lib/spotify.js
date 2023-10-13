dc13f1f9ce5ccad15966479c22b562fe690f3131
Nobel Zhou <nobelzhou19@gmail.com>
2023-10-13 18:49:11 -0400

    Add algorithm.py

--- timbre/src/lib/spotify.js
+++ timbre/src/lib/spotify.js
@@ -1,5 +1,9 @@
 const PLAYLISTS_ENDPOINT = 'https://api.spotify.com/v1/me/playlists';
 const TOP_TRACKS_ENDPOINT = 'https://api.spotify.com/v1/me/top/tracks';
+const TOP_ARTISTS_ENDPOINT = 'https://api.spotify.com/v1/me/top/artists';
+const RECENTLY_PLAYED_ENDPOINT = 'https://api.spotify.com/v1/me/player/recently-played';
+const TOP_ARTIST_TRACKS_ENDPOINT = 'https://api.spotify.com/v1/artists/{id}/top-tracks'; // Change for id
+const TRACK_FEATURES_REQUEST = 'https://api.spotify.com/v1/audio-features';
 
 export const getUsersPlaylists = async (refresh_token) => {
     const { access_token } = await getAccessToken(refresh_token);
@@ -21,3 +25,12 @@ export const topTracks = async (refresh_token) => {
     });
 };
 
+export const topArtists = async (refresh_token) => {
+    const { access_token } = await getAccessToken(refresh_token);
+
+    return fetch(TOP_ARTISTS_ENDPOINT, {
+        headers: {
+            Authorization: `Bearer ${access_token}`,
+        },
+    });
+};
