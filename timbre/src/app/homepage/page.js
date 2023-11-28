16613c79483d1084b09c50d22aaed7e8cb7a29fc
David Frost <82253398+davidhfrost@users.noreply.github.com>
2023-11-28 12:14:17 -0500

    matches and friends on homepage

--- timbre/src/app/homepage/page.js
+++ timbre/src/app/homepage/page.js
@@ -20,8 +20,19 @@ export default function Home() {
   const dummyFriends = [
     { id: 1, name: "Friend A", email: "frienda@email.com", profilePictureUrl: "path/to/imageA.jpg" },
     { id: 2, name: "Friend B", email: "friendb@email.com", profilePictureUrl: "path/to/imageB.jpg" },
+    { id: 3, name: "Friend C", email: "friendc@email.com", profilePictureUrl: "path/to/imageC.jpg" },
+    { id: 4, name: "Friend D", email: "friendd@email.com", profilePictureUrl: "path/to/imageD.jpg" },
     // ... add more friends as needed
   ];
+
+  const dummyMatches = [
+    { id: 1, name: "Match A", email: "matcha@email.com", profilePictureUrl: "path/to/imageM1.jpg" },
+    { id: 2, name: "Match B", email: "matchb@email.com", profilePictureUrl: "path/to/imageM2.jpg" },
+    { id: 3, name: "Match C", email: "matchc@email.com", profilePictureUrl: "path/to/imageM3.jpg" },
+    { id: 4, name: "Match D", email: "matchd@email.com", profilePictureUrl: "path/to/imageM4.jpg" }
+    // ... add more matches as needed
+  ];
+
   const [codeVerifier, setCodeVerifier] = useState("");
   const [access_token, setAccessToken] = useState("");
   //const [userTopArtists, setTopArtists] = useState([]);
@@ -51,7 +62,7 @@ export default function Home() {
       console.error("An error occurred while fetching top tracks:", error);
     }
   };
-  
+
   // Runs once when accessing this webpage. Fetches the user's top tracks
   useEffect(() => {
     if (!access_token) {
@@ -77,43 +88,84 @@ export default function Home() {
         <title>Timbre</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <Navbar bg="dark" variant="dark">
-        <Container fluid>
-          <Navbar.Brand href="/homepage">
-            Timbre
-          </Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="/matches">Matches</Nav.Link>
-            <Nav.Link href="/friends">Friends</Nav.Link>
-          </Nav>
-          <Button onClick={authorizeApp}>{isAuthenticated ? "Refresh Token" : "Sign In"}</Button>
-        </Container>
-      </Navbar>
+      <Navigation isAuthenticated={isAuthenticated} authorizeApp={authorizeApp} />
       <Button onClick={test}>Test API Endpoint</Button>
-      <div>
-        <Card>
-          <Card.Title>Your Top Tracks</Card.Title>
-          <Container>
-            <Row>
-              {
-                userTopTracks.map(track => (
-                  <Col md={3} key={track.id}>
-                    <Card>
-                      <Card.Img variant="top" src={track.album.images[0]?.url || ''} />
-                      <Card.Body>
-                        <Card.Title>{track.name}</Card.Title>
-                        <Card.Text>
-                          {track.artists.map(artist => artist.name).join(', ')}
-                        </Card.Text>
-                      </Card.Body>
-                    </Card>
-                  </Col>
-                ))
-              }
-            </Row>
-          </Container>
-        </Card>
-      </div>
+
+      <Container>
+        <Row className="mb-4">
+          <Col>
+            <Card>
+              <Card.Body>
+                <Card.Title>Welcome to Timbre</Card.Title>
+                <Card.Text>
+                  Connect with your music matches and explore new tracks!
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={6}>
+            <Card>
+              <Card.Title>Matches</Card.Title>
+              <ListGroup variant="flush" style={{ maxHeight: '5em', overflowY: 'scroll' }}>
+                {dummyMatches.map(match => (
+                  <ListGroup.Item key={match.id} href={`/profile/${match.id}`}>
+                    <img src={match.profilePictureUrl} alt={match.name} />
+                    {match.name}
+                  </ListGroup.Item>
+                ))}
+              </ListGroup>
+            </Card>
+          </Col>
+          <Col md={6}>
+            <Card>
+              <Card.Title>Friends</Card.Title>
+              <ListGroup variant="flush" style={{ maxHeight: '5em', overflowY: 'scroll' }}>
+                {dummyFriends.map(friend => (
+                  <ListGroup.Item key={friend.id} href={`/profile/${friend.id}`}>
+                    <img src={friend.profilePictureUrl} alt={friend.name} />
+                    {friend.name}
+                  </ListGroup.Item>
+                ))}
+              </ListGroup>
+            </Card>
+          </Col>
+        </Row>
+        {/* Add some space between the two sections */}
+        <Row className="mb-4">
+          <Col>
+            <br />
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12}>
+            <Card>
+              <Card.Title>Your Top Tracks</Card.Title>
+              <Container>
+                <Row>
+                  {
+                    userTopTracks.map(track => (
+                      <Col md={3} key={track.id}>
+                        <Card>
+                          <Card.Img variant="top" src={track.album.images[0]?.url || ''} />
+                          <Card.Body>
+                            <Card.Title>{track.name}</Card.Title>
+                            <Card.Text>
+                              {track.artists.map(artist => artist.name).join(', ')}
+                            </Card.Text>
+                          </Card.Body>
+                        </Card>
+                      </Col>
+                    ))
+                  }
+                </Row>
+              </Container>
+            </Card>
+            {/* ... [Top Tracks section remains unchanged] */}
+          </Col>
+        </Row>
+      </Container>
     </div>
   )
 }
