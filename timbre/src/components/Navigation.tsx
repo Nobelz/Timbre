c283fb9dfddb41c36188b232f869e54df6931b5c
Nobel Zhou <nobelzhou19@gmail.com>
2023-12-02 21:56:22 -0500

    Change call to database in Navigation.tsx

--- timbre/src/components/Navigation.tsx
+++ timbre/src/components/Navigation.tsx
@@ -7,7 +7,6 @@ import Button from 'react-bootstrap/Button';
 import Dropdown from 'react-bootstrap/Dropdown';
 import Nav from 'react-bootstrap/Nav';
 import Link from 'next/link';
-import { authorize, getToken } from "../api/auth/authorize";
 import { useEffect, useState } from 'react';
 
 
@@ -16,7 +15,8 @@ import { useEffect, useState } from 'react';
 // profilePage is true when the user is on the profile page
 
 const Navigation = ({ isAuthenticated, setIsAuthenticated, setAccessToken, authorizeApp, accessToken }) => {
-
+  
+  const [spotifyID, setSpotifyID] = useState('');
   const getInitials = (name) => {
     // if name, then first character, else empty string
     return name &&
@@ -32,73 +32,88 @@ const Navigation = ({ isAuthenticated, setIsAuthenticated, setAccessToken, autho
     localStorage.removeItem('token_type');
     localStorage.removeItem('scope');
     localStorage.removeItem('isAuthenticated');
+    localStorage.removeItem('spotify_id');
     localStorage.removeItem('userProfile');
     setIsAuthenticated(false);
     setAccessToken(null);
     console.log("Logged out");
   }
 
-  const [userProfile, setUserProfile] = React.useState(null);
+  const [userProfile, setUserProfile] = useState(null);
+
+  const fetchUserProfile = async () => {
+    // Fetch user profile using the access_token
+    try {
+      // const response = await fetch('https://api.spotify.com/v1/me', {
+      //   headers: { Authorization: `Bearer ${accessToken}` },
+      // });
+      // const data = await response.json();
+      let data = {
+        command: 'GET_USER_PROFILE',
+        spotify_id: spotifyID,
+      };
+
+      const response = await fetch('../api/endpoint', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(data),
+      });
+
+      const res = await response.json();
+      
+      setUserProfile(res.data.rows[0]);
+    } catch (error) {
+      console.error("Error fetching user profile:", error);
+    }
+  };
 
   useEffect(() => {
-    const fetchUserProfile = async () => {
-      if (accessToken) {
-        // Fetch user profile using the access_token
-        try {
-          const response = await fetch('https://api.spotify.com/v1/me', {
-            headers: { Authorization: `Bearer ${accessToken}` },
-          });
-          const data = await response.json();
-          setUserProfile(data);
-        } catch (error) {
-          console.error("Error fetching user profile:", error);
-        }
-      }
-    };
+    if (!spotifyID) {
+      setSpotifyID(localStorage.getItem('spotify_id') || '');
+    } else {
+      fetchUserProfile();
+    }
+  }, [accessToken, spotifyID]);
 
-    fetchUserProfile();
-  }, [accessToken]);
-
-  
-
-
-return (
-  <Navbar bg="dark" variant="dark">
-    <Container fluid>
-      <Navbar.Brand href="/homepage">
-        Timbre
-      </Navbar.Brand>
-      <Nav className="me-auto">
-        <Nav.Link href="/matches">Matches</Nav.Link>
-        <Nav.Link href="/friends">Friends</Nav.Link>
-      </Nav>
-
-      {isAuthenticated && userProfile && (
-        <Nav>
-          {userProfile?.images?.length > 0 ? (
-            <img src={userProfile.images[0].url} alt="Profile" style={{ borderRadius: '50%', width: '40px' }} />
-          ) : (
-            // make it white so it's visible on the dark navbar
-            <div style={{ backgroundColor: 'white', borderRadius: '50%', width: '40px', height: '40px', textAlign: 'center', lineHeight: '40px', fontSize: '20px' }}>
-              {getInitials(userProfile.display_name)}</div>
-          )}
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Container fluid>
+        <Navbar.Brand href="/homepage">
+          Timbre
+        </Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="/matches">Matches</Nav.Link>
+          <Nav.Link href="/friends">Friends</Nav.Link>
         </Nav>
 
-      )}
-      <Dropdown>
-        <Dropdown.Toggle variant="success" id="dropdown-basic">
-          Account
-        </Dropdown.Toggle>
+        {isAuthenticated && userProfile && (
+          <Nav>
+            {userProfile?.pic_link ? (
+              <img src={userProfile.pic_link} alt="Profile" style={{ borderRadius: '50%', width: '40px' }} />
+            ) : (
+              // make it white so it's visible on the dark navbar
+              <div style={{ backgroundColor: 'white', borderRadius: '50%', width: '40px', height: '40px', textAlign: 'center', lineHeight: '40px', fontSize: '20px' }}>
+                {getInitials(userProfile.display_name)}</div>
+            )}
+          </Nav>
 
-        <Dropdown.Menu>
-          <Dropdown.Item href="/profile"><Link href="/profile"><Button>My Profile</Button></Link></Dropdown.Item>
-          {/* Log out button */}
-          <Dropdown.Item><Button onClick={logout}>Log Out</Button></Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
-    </Container>
-  </Navbar>
-);
+        )}
+        <Dropdown>
+          <Dropdown.Toggle variant="success" id="dropdown-basic">
+            Account
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item href="/profile"><Link href="/profile"><Button>My Profile</Button></Link></Dropdown.Item>
+            {/* Log out button */}
+            <Dropdown.Item><Button onClick={logout}>Log Out</Button></Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Navigation;
