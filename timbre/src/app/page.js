e50ddafa728fcddfd78173ad2fee94b7a7aca4fb
David Frost <82253398+davidhfrost@users.noreply.github.com>
2023-12-02 19:21:10 -0500

    redirect from login if authenticated

--- timbre/src/app/page.js
+++ timbre/src/app/page.js
@@ -6,18 +6,36 @@ import Button from 'react-bootstrap/Button';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Navigation from '../components/Navigation';
+import useAuthentication from '../hooks/useAccessToken';
+import useAuthRedirect from '../hooks/useAuthRedirect';
+import useUserProfile from '../hooks/useUserProfile';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 /* 
  The landing page of the application where users will have to sign in to their Spotify account.
  */
 export default function Login() {
+  const { access_token, isAuthenticated, setAccessToken, setIsAuthenticated } = useAuthentication();
+
+  const userProfile = useUserProfile(access_token);
+
+  const isLoading = useAuthRedirect(isAuthenticated);
+
+  const router = useRouter();
+
+  useEffect (() => {
+    if (isAuthenticated && !isLoading) {
+      router.push('/homepage');
+    }
+  }, [isAuthenticated]);
   return (
     <div>
       <Head>
         <title>Timbre</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-    <Navigation isAuthenticated={false} authorizeApp={authorize} />
+      <Navigation isAuthenticated={false} authorizeApp={authorize} />
     </div>
   )
 }
