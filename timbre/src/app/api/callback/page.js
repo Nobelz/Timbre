309ae66f23a7d0ff99384839424c3349de5caa67
Devin Lin <dxl702@case.edu>
2023-12-04 16:35:50 -0500

    fixed some warnings

--- timbre/src/app/api/callback/page.js
+++ timbre/src/app/api/callback/page.js
@@ -21,16 +21,16 @@ export default function Callback() {
     <Container>
       <Row style={{paddingTop: '15%'}}> 
         <Col>
-          <div class={`${styles.center}`}>
-          <div class={`${styles.wave}`}></div>
-          <div class={`${styles.wave}`}></div>
-          <div class={`${styles.wave}`}></div>
-          <div class={`${styles.wave}`}></div>
-          <div class={`${styles.wave}`}></div>
-          <div class={`${styles.wave}`}></div>
-          <div class={`${styles.wave}`}></div>
-          <div class={`${styles.wave}`}></div>
-          <div class={`${styles.wave}`}></div>
+          <div className={`${styles.center}`}>
+          <div className={`${styles.wave}`}></div>
+          <div className={`${styles.wave}`}></div>
+          <div className={`${styles.wave}`}></div>
+          <div className={`${styles.wave}`}></div>
+          <div className={`${styles.wave}`}></div>
+          <div className={`${styles.wave}`}></div>
+          <div className={`${styles.wave}`}></div>
+          <div className={`${styles.wave}`}></div>
+          <div className={`${styles.wave}`}></div>
 
         </div>
         <link href='https://fonts.googleapis.com/css?family=Lexend' rel='stylesheet'/>
