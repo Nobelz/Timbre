b1a6cfca08b005e0bf60292c23ba9f5d2e2e1665
Nobel Zhou <nobelzhou19@gmail.com>
2023-12-03 20:36:47 -0500

    Update endpoint

--- timbre/src/app/api/endpoint/route.js
+++ timbre/src/app/api/endpoint/route.js
@@ -4,52 +4,45 @@ import { NextResponse } from 'next/server';
 import { getTop3Matches } from "../../../lib/matching"
 
 // Handler for PUT requests 
-// Can potentially differentiate between which functions to call in db_functions using request.json() or request.text()
-// That will get the body of the fetch request from the frontend: line 50 of homepage/page.js
-// There might be a better method
-
-// TODO fix the response garbage, it's possibly causing errors
 export async function PUT(request) {
     const body = await request.json();
     try {
-        let response;
-        let response1;
-        let response2;
-        let response3;
         switch (body.command) {
             case 'GENERATE_SPOTIFY_DATA':
                 /*
                     access_token: The API access token
                 */
-                response = await generateSpotifyData(body.access_token);
-                break;
+                let spotifyDataResponse = await generateSpotifyData(body.access_token);
+                return NextResponse.json({ message: 'Successful PUT request', success: true, data: spotifyDataResponse });
             case 'DENY_FRIEND_REQUEST':
                 /*
                     current_id: The Spotify ID of the current user
                     friend_id: The Spotify ID of the friend whose request is being denied
                 */
-                response1 = await getUserIDFromSpotifyID(body.current_id);
-                response2 = await getUserIDFromSpotifyID(body.friend_id);
-                response = await rejectFriendRequest(response1.rows[0].search_user_from_id, response2.rows[0].search_user_from_id);
-                break;
+                let denyResponse1 = await getUserIDFromSpotifyID(body.current_id);
+                let denyResponse2 = await getUserIDFromSpotifyID(body.friend_id);
+                let denyResponse = await rejectFriendRequest(denyResponse1.rows[0].search_user_from_id, denyResponse2.rows[0].search_user_from_id);
+                return NextResponse.json({ message: 'Successful PUT request', success: true, data: denyResponse });
             case 'ACCEPT_FRIEND_REQUEST':
                 /*
                     current_id: The Spotify ID of the current user
                     friend_id: The Spotify ID of the friend whose request is being accepted
                 */
-                response1 = await getUserIDFromSpotifyID(body.current_id);
-                response2 = await getUserIDFromSpotifyID(body.friend_id);
-                response = await acceptFriendRequest(response1.rows[0].search_user_from_id, response2.rows[0].search_user_from_id);
+                let acceptResponse1 = await getUserIDFromSpotifyID(body.current_id);
+                let acceptResponse2 = await getUserIDFromSpotifyID(body.friend_id);
+                let acceptResponse = await acceptFriendRequest(acceptResponse1.rows[0].search_user_from_id, acceptResponse2.rows[0].search_user_from_id);
+                return NextResponse.json({ message: 'Successful PUT request', success: true, data: acceptResponse });
             case 'MAKE_FRIEND_REQUEST':
                 /*
                     current_id: The Spotify ID of the current user
                     email: The email address of the potential friend
                 */
-                response1 = await getUserIDFromEmail(body.email);
-                response2 = await getUserIDFromSpotifyID(body.current_id);
-                if (response1) {
-                    if (response1.rows[0].search_user_from_email === response2.rows[0].search_user_from_id) {
-                        response = {
+                let friendReqResponse1 = await getUserIDFromEmail(body.email);
+                let friendReqResponse2 = await getUserIDFromSpotifyID(body.current_id);
+                let friendReqResponse;
+                if (friendReqResponse1) {
+                    if (friendReqResponse1.rows[0].search_user_from_email === friendReqResponse2.rows[0].search_user_from_id) {
+                        friendReqResponse = {
                             data:
                             {
                                 code: 400,
@@ -57,9 +50,9 @@ export async function PUT(request) {
                             }
                         }
                     } else {
-                        response3 = await checkFriends(response1.rows[0].search_user_from_email, response2.rows[0].search_user_from_id);
-                        if (response3.rows[0].check_friends) {
-                            response = {
+                        let friendReqResponse3 = await checkFriends(friendReqResponse1.rows[0].search_user_from_email, friendReqResponse2.rows[0].search_user_from_id);
+                        if (friendReqResponse3.rows[0].check_friends) {
+                            friendReqResponse = {
                                 data:
                                 {
                                     code: 400,
@@ -67,11 +60,11 @@ export async function PUT(request) {
                                 }
                             };
                         } else {
-                            response = await makeFriendRequest(response2.rows[0].search_user_from_id, response1.rows[0].search_user_from_email);
+                            friendReqResponse = await makeFriendRequest(friendReqResponse2.rows[0].search_user_from_id, friendReqResponse1.rows[0].search_user_from_email);
                         }
                     }
                 } else {
-                    response = {
+                    friendReqResponse = {
                         data:
                         {
                             code: 404,
@@ -79,53 +72,52 @@ export async function PUT(request) {
                         }
                     };
                 }
-                break;
+                return NextResponse.json({ message: 'Successful PUT request', success: true, data: friendReqResponse.data });
             case 'MAKE_FRIEND_REQUEST_WITH_ID':
-                response1 = await getUserIDFromSpotifyID(body.match_id);
-                response2 = await getUserIDFromSpotifyID(body.current_id);
-                response = await makeFriendRequest(response2.rows[0].search_user_from_id, response1.rows[0].search_user_from_id);
-                break;
+                /*
+                    match_id: The Spotify ID of the user to be added as a friend
+                    current_id: The Spotify ID of the current user
+                */
+                let makeFriendResponse1 = await getUserIDFromSpotifyID(body.match_id);
+                let makeFriendResponse2 = await getUserIDFromSpotifyID(body.current_id);
+                let makeFriendResponse = await makeFriendRequest(makeFriendResponse2.rows[0].search_user_from_id, makeFriendResponse1.rows[0].search_user_from_id);
+                return NextResponse.json({ message: 'Successful PUT request', success: true, data: makeFriendResponse });
             case 'MAKE_RECOMMENDATION':
                 /*
                     current_id: The Spotify ID of the current user
                     friend_id: The User ID of the friend receiving the recommendation
                     song: The song being recommended
                 */
-                response1 = await getUserIDFromSpotifyID(body.current_id);
-                response = await makeRecommendation(response1.rows[0].search_user_from_id, body.friend_id, body.song);
-                break;
+                let recResponse1 = await getUserIDFromSpotifyID(body.current_id);
+                let recResponse = await makeRecommendation(recResponse1.rows[0].search_user_from_id, body.friend_id, body.song);
+                return NextResponse.json({ message: 'Successful PUT request', success: true, data: recResponse });
             case 'UPDATE_BIO':
                 /*
                     spotify_id: The Spotify ID of the current user
                     new_bio: The bio to update to
                 */
-                response1 = await getUserIDFromSpotifyID(body.spotify_id);
-                response = await updateUserBio(response1.rows[0].search_user_from_id, body.new_bio);
-                break;
+                let bioResponse1 = await getUserIDFromSpotifyID(body.spotify_id);
+                let bioResponse = await updateUserBio(bioResponse1.rows[0].search_user_from_id, body.new_bio);
+                return NextResponse.json({ message: 'Successful PUT request', success: true, data: bioResponse });
             case 'RATE_SONG':
-                response1 = await getUserIDFromSpotifyID(body.spotify_id);
-                response = await insertSongRating(response1.rows[0].search_user_from_id, body.track_id, body.rating);
-                break;
+                let rateResponse1 = await getUserIDFromSpotifyID(body.spotify_id);
+                let rateResponse = await insertSongRating(rateResponse1.rows[0].search_user_from_id, body.track_id, body.rating);
+                return NextResponse.json({ message: 'Successful PUT request', success: true, data: rateResponse });
             default:
                 return NextResponse.json({ message: 'Internal server error', success: false });
         }
-        return NextResponse.json({ message: 'Successful PUT request', success: true, data: response.data });
+        return NextResponse.json({ message: 'Internal server error', success: false });
     } catch (err) {
         return NextResponse.json({ message: 'Internal server error', success: false, error: err });
     }
-
     return NextResponse.json({ message: 'Internal server error', success: false });
 }
 
+// Handler for POST requests
 export async function POST(request) {
     const body = await request.json();
 
     try {
-        let response;
-        let response1;
-        let response2;
-        let response3;
-        let response4;
         switch (body.command) {
             case 'CALCULATE_COMPATIBILITY':
                 /*
@@ -133,33 +125,33 @@ export async function POST(request) {
                     id2: The Spotify ID of the second user
                     Returns: The compatibility score between the two users
                 */
-                response = await calculateCompatibilityScore(body.id1, body.id2);
-                break;
+                let compatResponse = await calculateCompatibilityScore(body.id1, body.id2);
+                return NextResponse.json({ message: 'Successful POST request', success: true, data: compatResponse });
             case 'GET_FRIENDS':
                 /*
                     spotify_id: The Spotify ID of the current user
                     Returns: The friends of the current user
                 */
-                response1 = await getUserIDFromSpotifyID(body.spotify_id);
-                response = await getFriends(response1.rows[0].search_user_from_id);
-                break;
+                let getFriendsResponse1 = await getUserIDFromSpotifyID(body.spotify_id);
+                let getFriendsResponse = await getFriends(getFriendsResponse1.rows[0].search_user_from_id);
+                return NextResponse.json({ message: 'Successful POST request', success: true, data: getFriendsResponse });
             case 'GET_FRIEND_REQUESTS':
                 /*
                     spotify_id: The Spotify ID of the current user
                     Returns: The friend requests of the current user
                 */
-                response1 = await getUserIDFromSpotifyID(body.spotify_id);
-                response = await getFriendRequests(response1.rows[0].search_user_from_id);
-                break;
+                let friendReqResponse1 = await getUserIDFromSpotifyID(body.spotify_id);
+                let friendReqResponse = await getFriendRequests(friendReqResponse1.rows[0].search_user_from_id);
+                return NextResponse.json({ message: 'Successful POST request', success: true, data: friendReqResponse });
             case 'GET_RECOMMENDATIONS':
                 /*
                     spotify_id: The Spotify ID of the current user
                     Returns: The recommendations of the current user, along with the user that recommended them
                 */
-                response1 = await getUserIDFromSpotifyID(body.spotify_id);
-                response2 = await getRecommendations(response1.rows[0].search_user_from_id);
+                let recResponse1 = await getUserIDFromSpotifyID(body.spotify_id);
+                let recResponse2 = await getRecommendations(recResponse1.rows[0].search_user_from_id);
             
-                let recIDs = response2.rows.map((rec) => rec.song_id);
+                let recIDs = recResponse2.rows.map((rec) => rec.song_id);
                 recIDs = Array.from(new Set(recIDs)); // Remove recommendation duplicates
                 recIDs = recIDs.slice(0, 20); 
 
@@ -168,17 +160,17 @@ export async function POST(request) {
                     let track = {};
                     track.song_id = id;
 
-                    response3 = await getSongInformation(response1.rows[0].search_user_from_id, id);
-                    let songInfo = response3.rows[0];
+                    let recResponse3 = await getSongInformation(recResponse1.rows[0].search_user_from_id, id);
+                    let songInfo = recResponse3.rows[0];
                     track.title = songInfo.title;
                     track.uri = songInfo.uri;
                     track.albumImageUrl = songInfo.album_image_url;
                     track.rating = parseFloat(songInfo.rating);
 
-                    response4 = await getSongArtistInformation(id);
+                    let recResponse4 = await getSongArtistInformation(id);
                     track.artists = [];
                     track.artist_ids = [];
-                    let artistInfo = response4.rows;
+                    let artistInfo = recResponse4.rows;
                     for (const artist of artistInfo) {
                         track.artists.push(artist.artist_name);
                         track.artist_ids.push(artist.artist_id);
@@ -187,37 +179,33 @@ export async function POST(request) {
                     recommendedTracks.push(track);
                 }
 
-                response = {tracks: recommendedTracks};
-                break;
+                let recResponse = {tracks: recommendedTracks};
+                return NextResponse.json({ message: 'Successful POST request', success: true, data: recResponse });
             case 'GET_USER_PROFILE':
                 /*
                     spotify_id: The Spotify ID of the current user
                     Returns: The profile of the current user
                 */
-                response = await getUserInfo(body.spotify_id);
-                break;
+                let profileResponse = await getUserInfo(body.spotify_id);
+                return NextResponse.json({ message: 'Successful POST request', success: true, data: profileResponse });
             case 'GET_MATCHES':
                 /*
                     spotify_id: The Spotify ID of the current user
                     Returns: The matches of the current user
                 */
-                response = await getTop3Matches(body.spotify_id);
-                break;
+                let matchResponse = await getTop3Matches(body.spotify_id);
+                return NextResponse.json({ message: 'Successful POST request', success: true, data: matchResponse });
             default:
                 return NextResponse.json({ message: 'Internal server error', success: false });
         }
-
-        return NextResponse.json({ message: 'Successful POST request', success: true, data: response });
+        return NextResponse.json({ message: 'Internal server error', success: false });
     } catch (err) {
         console.log(err);
         return NextResponse.json({ message: 'Internal server error', success: false, error: err });
     }
-
     return NextResponse.json({ message: 'Internal server error', success: false });
 }
 
 export async function GET(request) {
     return NextResponse.json({ message: 'Internal server error', success: false });
 }
-// look at this https://stackoverflow.com/questions/76214029/no-http-methods-exported-in-export-a-named-export-for-each-http-method
-// name each method POST, GET, etc...
